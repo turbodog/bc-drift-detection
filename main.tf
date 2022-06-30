e1c211bdacb1b885b3cabe353e3d4145e55e0fba
Lindsey Smith <lindsey.smith@gmail.com>
2022-06-30 09:48:51 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_security_group" "project-iac-sg" {
+  name = "tf-managed-sg"
+  description = "This security group is managed by Terraform"
+  vpc_id = "vpc-ae2fb8d6"
+
+  // To Allow Port 80 Transport
+  ingress {
+    from_port = 80
+    protocol = "TCP"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
